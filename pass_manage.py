dbfff565183102096ec5918c92a55ac0a8bf9c41
MaskedTitan <66492627+satharv@users.noreply.github.com>
2023-07-25 08:41:41 +0530

    Update pass_manage.py

--- pass_manage.py
+++ pass_manage.py
@@ -57,14 +57,14 @@ def check_password_strength(password):
 def store_master_password():
     # Get master password from user
     while True:
-        master_password = getpass.getpass("Create a master password: ")
+        master_password = getpass.getpass("\033[92mCreate a master password:\033[0m ")
         strength = check_password_strength(master_password)
         if strength < 4:
-            print("Weak password. Try again.")
+            print("\033[91mWeak password. Try again.\033[0m")
         else:
             break
 
-    confirm_password = getpass.getpass("Confirm your master password: ")
+    confirm_password = getpass.getpass("\033[92mConfirm your master password:\033[0m ")
 
     if master_password != confirm_password:
         print("\033[91m------------MASTER PASSWORDS DO NOT MATCH------------\033[0m")
@@ -77,7 +77,7 @@ def store_master_password():
     with open(MASTER_PASSWORD_FILE, "w") as f:
         f.write(hashed_password)
 
-    print("Master password has been set successfully!")
+    print("\033[92mMaster password has been set successfully!\033[0m")
 
 
 # Verify master password
@@ -98,7 +98,7 @@ def verify_master_password(entered_password):
         print("\033[91m------------MASTER PASSWORD IS INCORRECT------------\033[0m")
         sys.exit()
 
-    print("Master password verified successfully!")
+    print("\033[92mMaster password verified successfully!\033[0m")
 
 
 # Generate encryption key
@@ -140,16 +140,16 @@ def generate_password(length):
 
 # Store a password
 def store_password():
-    mst_pass = getpass.getpass("Enter master password: ")
+    mst_pass = getpass.getpass("\033[91mEnter master password: \033[0m")
     if master_password != mst_pass:
         print("\033[91m------------MASTER PASSWORD IS INCORRECT------------\033[0m")
         return
 
     # Get site, username, password
-    site: str = input("Enter site-name or email: ")
-    username = input("Enter your username: ")
+    site: str = input("\033[93mEnter site-name or email:\033[93m ")
+    username = input("\033[93mEnter your username:\033[93m ")
     # getpass.getpass is used as an input, but it will hide the password on terminal which we will enter
-    password = getpass.getpass("Enter your password: ")
+    password = getpass.getpass("\033[93mEnter your password:\033[0m ")
 
     if not os.path.exists("key.key"):
         generate_key()
@@ -160,13 +160,13 @@ def store_password():
     # open file by using with function
     with open("password.txt", 'a+') as f:
         f.write(f"{site}::{username}::{enc_password.decode()}\n")
-        print("!!!!!!!!!!!Password stored successfully!!!!!!!!!")
+        print("\033[92m!!!!!!!!!!!Password stored successfully!!!!!!!!!\033[0m")
 
 
 # Retrieve passwords
 def show_password():
     # Let user view decrypted passwords
-    mst_pass = getpass.getpass("Enter master password: ")
+    mst_pass = getpass.getpass("\033[91mEnter master password: \033[0m")
     if master_password != mst_pass:
         print("\033[91m------------MASTER PASSWORD IS INCORRECT------------\033[0m")
         return
@@ -187,33 +187,36 @@ def show_password():
         print("------------------------------------------------------------")
 
 
+
 # Main menu
 def main(mst_pass):
     # verify master password is present or not
     verify_master_password(mst_pass)
     # Show menu options in loop
     while True:
-        print("1. Store a password:")
-        print("2. Show password!")
-        print("3. Generate random password")
-        print("4. Quit")
+        print("\033[92m1. Store a password:\033[0m")
+        print("\033[92m2. Show password!\033[0m")
+        print("\033[92m3. Generate random password\033[0m")
+        print("\033[92m4. Quit\033[0m")
         # Call appropriate functions
-        choice = input("Enter your choice: ")
+        choice = input("\033[94mEnter your choice:\033[93m ")
         if choice == "1":
             store_password()
-        if choice == "2":
+        elif choice == "2":
             show_password()
-        if choice == "3":
+        elif choice == "3":
             password_length = int(input("Enter password length: "))
             password = generate_password(password_length)
-            print("Generated Password:", password)
-        if choice == "4":
+            print(f"Generated Password:'\033[91m{password}\033[0m'")
+        elif choice == "4":
             sys.exit()
+        else:
+            print("\033[91mPlease enter valid Input\033[0m")
 
 
 if not os.path.exists(MASTER_PASSWORD_FILE):
     store_master_password()
-master_password = getpass.getpass("Enter master password: ")
+master_password = getpass.getpass("\033[91mEnter master password:\033[0m ")
 # Start the program
 main(master_password)
 
